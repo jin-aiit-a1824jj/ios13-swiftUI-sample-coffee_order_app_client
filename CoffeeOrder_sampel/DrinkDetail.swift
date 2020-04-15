27534c354c1ebfaad3adf3674e4ec833da5d47d3
a1824jj <a1824jj@aiit.ac.jp>
2020-04-15 11:59:48 +0900

    add orderbutton in DrinkDetail

--- CoffeeOrder_sampel/DrinkDetail.swift
+++ CoffeeOrder_sampel/DrinkDetail.swift
@@ -13,6 +13,7 @@ struct DrinkDetail: View {
     var drink: Drink
     
     var body: some View {
+        
         ScrollView(.vertical, showsIndicators:  false) {
             ZStack(alignment: .bottom, content: {
                 Image(drink.imageName)
@@ -37,12 +38,45 @@ struct DrinkDetail: View {
                     Spacer()
                 }
             })
+            .listRowInsets(EdgeInsets())
+            Text(drink.description)
+                .foregroundColor(.primary)
+                .font(.body)
+                .lineLimit(5)
+                .padding()
+            HStack {
+                Spacer()
+                OrderButton(drink: self.drink)
+                Spacer()
+            }.padding(.top, 50)
+            
         }
+        .edgesIgnoringSafeArea(.top)
+        .navigationBarHidden(true)
     }
+    
 }
 
 struct DrinkDetail_Previews: PreviewProvider {
     static var previews: some View {
         DrinkDetail(drink: drinkData.first!)
     }
+}
+
+struct OrderButton : View {
+    
+    var drink: Drink
+    
+    var body : some View {
+        Button(
+            action: { print("Add to Basket, \(self.drink.name)") },
+            label: { Text("Add to Basket") }
+        )
+        .frame(width: 200, height: 50)
+        .foregroundColor(.white)
+        .font(.headline)
+        .background(Color.blue)
+        .cornerRadius(10)
+    }
+    
 }
