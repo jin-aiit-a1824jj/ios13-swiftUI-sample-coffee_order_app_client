d395623c31503c98df45ab6a9a7e4e44aeb8ee9b
a1824jj <a1824jj@aiit.ac.jp>
2020-04-15 10:10:25 +0900

    make project

---
+++ CoffeeOrder_sampel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeOrder_sampel
+//
+//  Created by JONGWOO JIN on 2020/04/15.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
