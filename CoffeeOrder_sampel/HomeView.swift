9fbfa98263766daaa874bab62dc6be84fe08cc80
a1824jj <a1824jj@aiit.ac.jp>
2020-04-15 10:55:54 +0900

    add menu in firebase

---
+++ CoffeeOrder_sampel/HomeView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  CoffeeOrder_sampel
+//
+//  Created by JONGWOO JIN on 2020/04/15.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        
+        NavigationView{
+            Text("Hello, World!")
+            .navigationBarTitle(Text("Coffee_sampel"))
+            .navigationBarItems(
+                leading:
+                    Button(action: {
+                        print("Log out")
+                        createMenu()
+                    }, label: {Text("Log Out")}),
+                trailing:
+                    Button(action: {
+                        print("Log out")
+                        
+                    }, label: {Text("Log Out")})
+            )
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
