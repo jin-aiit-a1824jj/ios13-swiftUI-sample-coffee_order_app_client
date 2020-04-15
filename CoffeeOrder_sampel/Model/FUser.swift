2c7482c67c0f3d18836d6b30bebb0619e7193cc1
a1824jj <a1824jj@aiit.ac.jp>
2020-04-15 16:27:03 +0900

    add FUser model

---
+++ CoffeeOrder_sampel/Model/FUser.swift
@@ -0,0 +1,61 @@
+//
+//  FUser.swift
+//  CoffeeOrder_sampel
+//
+//  Created by JONGWOO JIN on 2020/04/15.
+//  Copyright © 2020 JONGWOO JIN. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+class FUser {
+    
+    let id: String
+    var email = ""
+    var firstName = ""
+    var lastName = ""
+    var fullName = ""
+    var phoneNumber = ""
+    
+    var fullAddress = ""
+    var onBoarding = false
+
+    init(_id: String, email: String, firstName: String, lastName: String, fullName: String, phoneNumber: String ){
+        id = _id
+        self.email = email
+        self.firstName = firstName
+        self.lastName = lastName
+        self.fullName = firstName + " " + lastName
+        self.phoneNumber = phoneNumber
+        onBoarding = false
+    }
+    
+    class func currentId() -> String {
+        return Auth.auth().currentUser!.uid
+    }
+    
+    class func currentUser() -> FUser? {
+        if Auth.auth().currentUser != nil {
+            if let dictionary = userDefaults.object(forKey: kCURRENTUSER) {
+                return nil//FUser(_id: <#T##String#>, email: <#T##String#>, firstName: <#T##String#>, lastName: <#T##String#>, fullName: <#T##String#>, phoneNumber: <#T##String#>)
+            }
+        }
+        return nil
+    }
+    
+    class func loginUserWith(email: String, password: String, completion: @escaping (_ error: Error?, _ isEmailVerified: Bool) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password){ (authDataResult, error) in
+            if error == nil {
+                if authDataResult!.user.isEmailVerified {
+                    
+                } else {
+                    completion(error, false)
+                }
+            }else {
+                completion(error, false)
+            }
+        }
+    }
+    
+}
